a52193ef96f0e758b34ca3796db44325cc762895
gracebir <grace.devolop@gmail.com>
2023-06-29 00:01:41 +0200

    structured the page

---
+++ client/src/pages/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import ChatArea from '../components/ChatArea'
+import TextMessage from '../components/TextMessage'
+
+function Message() {
+  return (
+    <div className='bg-gray-950 h-screen w-full'>
+      <main className='max-w-3xl bg-white mx-auto font-inter flex flex-col h-screen relative'>
+        <Header />
+        <ChatArea />
+        <TextMessage />
+      </main>
+    </div>
+  )
+}
+
+export default Message
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {signInWithGoogle} from '../Firebase'
+import googleLogo from '../assets/google_logo.svg'
+
+function SignIn() {
+  return (
+    <div className='h-screen bg-gray-color flex items-center font-poppins'>
+      <div className="max-w-sm rounded-lg h-[50vh] items-center flex flex-col gap-6 flex-1 bg-white shadow-lg mx-auto px-6 py-4">
+        <div className='flex flex-col gap-4'>
+          <h1 className="text-center text-xl font-bold text-head-color uppercase">Sign In</h1>
+          <span className='text-gray-500 text-center text-sm'>Log In first with Google before accessing the chat</span>
+        </div>
+        <div className='lg:w-64 lg:h-64 w-48 h-48'>
+          <img className='w-full h-full' src={googleLogo} alt="googlelogo" />
+        </div>
+        <button onClick={()=> signInWithGoogle()} className='bg-head-color w-full py-2 rounded-md text-gray-color font-semibold'>LogIn</button>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
