06fbc4f0f330c2449bfb9db73fa6a15e983b6aaa
gracebir <grace.devolop@gmail.com>
2023-06-28 23:58:36 +0200

    adding user to the context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,18 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const initialState = {
     messages: [],
-    setMessages: null
+    setMessages: null,
+    user: "",
+    setUser: null
 }
 
 export const AppContext = createContext(initialState)
 
 export const AppProvider = ({children}) => {
     const [messages, setMessages] = useState([])
+    const [user, setUser] = useState("")
+    useEffect(()=> {
+        const getMessages = async () => {
+            const response = await axios.get(`http://localhost:4000/messages/${user ? user : 'Grace Birindwa'}`)
+            const data = await response.data
+            setMessages(data)
+        }
+        getMessages()
+    }, [user])
     return (
         <AppContext.Provider value={{
             messages: messages,
-            setMessages: setMessages
+            setMessages: setMessages,
+            user: user, 
+            setUser: setUser
         }}>
             {children}
         </AppContext.Provider>
