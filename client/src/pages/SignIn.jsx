0cddbdcdde8b55de95f00965189f70eec69be22b
gracebir <grace.devolop@gmail.com>
2023-06-30 23:58:55 +0200

    design in and sign up page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,24 @@
 import React, { useContext } from 'react'
-import {signInWithGoogle} from '../Firebase'
+import { signInWithGoogle } from '../Firebase'
 import googleLogo from '../assets/google_logo.svg'
+import logo from '../assets/logo1.svg'
 import { AppContext } from '../context/AppContext'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import TextField from '../components/TextField'
 
 function SignIn() {
   const navigate = useNavigate()
-  const {setUser, setIsLogged} = useContext(AppContext)
+  const { setUser, setIsLogged } = useContext(AppContext)
+  const { values, handleChange, handleBlur, errors } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: () => {
+
+    }
+  })
   const handleSignIn = async () => {
     const data = await signInWithGoogle()
     setUser(data.displayName)
@@ -16,16 +28,33 @@ function SignIn() {
     localStorage.setItem("isLogged", true)
   }
   return (
-    <div className='h-screen bg-gray-color flex items-center font-poppins'>
-      <div className="max-w-sm rounded-lg h-[50vh] items-center flex flex-col gap-6 flex-1 bg-white shadow-lg mx-auto px-6 py-4">
-        <div className='flex flex-col gap-4'>
-          <h1 className="text-center text-xl font-bold text-head-color uppercase">Sign In</h1>
-          <span className='text-gray-500 text-center text-sm'>Log In first with Google before accessing the chat</span>
+    <div className='w-full h-screen flex items-center font-poppins'>
+      <div className='max-w-xl mx-auto px-4 lg:px-0 flex-1 flex flex-col gap-6'>
+        <div className='flex gap-2'>
+          <img src={logo} alt="logo" />
+          <div className='flex flex-col'>
+            <span className='text-head-color text-lg font-semibold'>Elastic Team</span>
+            <span className='text-sm font-semibold'>Open AI - Text Generator</span>
+          </div>
         </div>
-        <div className='lg:w-64 lg:h-64 w-48 h-48'>
-          <img className='w-full h-full' src={googleLogo} alt="googlelogo" />
-        </div>
-        <button onClick={()=> handleSignIn()} className='bg-head-color w-full py-2 rounded-md text-gray-color font-semibold'>LogIn</button>
+        <h3 className='font-semibold'>Please log in to continue</h3>
+        <form className="flex flex-col gap-4">
+          <TextField name={"email"} label={"Email"} value={values.email} onChange={handleChange} type={"email"} />
+          <TextField name="password" label={"Password"} value={values.password} onChange={handleChange} type={"password"} />
+          <div className='mt-4'>
+            <div className='flex  flex-col-reverse gap-2'>
+              <div className='flex gap-4'>
+                <button className='bg-button-color hover:bg-button-color px-5 py-2 text-white font-semibold rounded-lg'>
+                  Sign Up
+                </button>
+                <span className="w-10 h-10 border rounded-lg cursor-pointer">
+                  <img className='w-full' src={googleLogo} alt="googlelogo" />
+                </span>
+              </div>
+              <span className='text-sm'>Don't have an Account. <Link className='text-button-color underline' to={"/signup"}>Sign Up</Link></span>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   )
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import TextField from '../components/TextField'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo1.svg'
+
+function SignUp() {
+    const { values, handleChange, handleBlur, errors } = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        onSubmit: () => {
+
+        }
+    })
+
+    return (
+        <div className='w-full h-screen flex items-center font-poppins'>
+            <div className='max-w-xl mx-auto px-4 lg:px-0 flex-1 flex flex-col gap-6'>
+                <div className='flex gap-2'>
+                    <img src={logo} alt="logo" />
+                    <div className='flex flex-col'>
+                        <span className='text-head-color font-semibold'>Elastic Team</span>
+                        <span className='text-sm font-semibold'>Open AI - Text Generator</span>
+                    </div>
+                </div>
+                <h3 className='font-semibold'>Create an account here</h3>
+                <form className="flex flex-col gap-4">
+                    <TextField name={"name"} label={"Name"} value={values.name} onChange={handleChange} type={"text"} />
+                    <TextField name={"email"} label={"Email"} value={values.email} onChange={handleChange} type={"email"} />
+                    <TextField name="password" label={"Password"} value={values.password} onChange={handleChange} type={"password"} />
+                    <TextField name="confirmPassword" label={"Confirm password"} value={values.confirmPassword} onChange={handleChange} type={"password"} />
+                    <div className='flex justify-between items-end'>
+                        <button className='bg-button-color hover:bg-button-color px-5 py-2 text-white font-semibold rounded-lg'>
+                            Sign Up
+                        </button>
+                        <span className='text-sm'>Have an account. <Link className='text-button-color underline' to={"/"}>Login</Link></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
