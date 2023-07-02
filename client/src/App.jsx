a6ab09e34c180d630c8b43d356eb0b6d16bcbdff
gracebir <grace.devolop@gmail.com>
2023-07-02 21:57:33 +0200

    add protected route to Message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import SignIn from './pages/SignIn'
-import Message from './pages/Message' 
+import Message from './pages/Message'
 import SignUp from './pages/SignUp'
+import Protected from './components/Protected'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<SignIn />} />
+        <Route path='/login' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/chat' element={
-          <Message />
+        <Route path='/' element={
+          <Protected>
+            <Message />
+          </Protected>
         } />
       </Routes>
     </>
