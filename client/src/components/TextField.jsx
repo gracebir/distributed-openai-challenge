db46c516a8949abc01db7952a4039eb60bb77b82
gracebir <grace.devolop@gmail.com>
2023-07-02 20:37:04 +0200

    text errors

--- client/src/components/TextField.jsx
+++ client/src/components/TextField.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const TextField = ({label, type,onChange,name, value, errorMsg, onBlur}) => {
+const TextField = ({label, type,onChange,name, value, errorMsg,touched, onBlur}) => {
   return (
     <div className='flex flex-col gap-1'>
       <label className='text-base text-[#2D3142] font-normal'>{label}</label>
-      <input className={`outline-none py-2 px-3 border border-gray-300 rounded-lg`} name={name} type={type} value={value} onChange={onChange} onBlur={onBlur}/>
-      {errorMsg && <span>{errorMsg}</span>}
+      <input className={`outline-none py-2 px-3 border ${errorMsg ? "border-red-400":"border-gray-300"} rounded-lg`} name={name} type={type} value={value} onChange={onChange} onBlur={onBlur}/>
+      {errorMsg && touched && <span className='text-red-400 text-sm'>{errorMsg}</span>}
     </div>
   )
 }
